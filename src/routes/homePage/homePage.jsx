a557c586cfc6a5d2e9db2b79609e8440132449f3
bob <marcellinus.cg@gmail.com>
2024-06-26 13:44:14 +0700

    ui-mern

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,40 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+            veniam eveniet, animi iusto placeat consectetur obcaecati neque iste
+            incidunt soluta, iure dolorum natus tempore ut nesciunt mollitia ex
+            adipisci similique!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
